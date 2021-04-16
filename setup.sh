15f0e55d9415130920038e4e02c6f2fc5abe89c2
Slow Abra <sabra@ga-o1.msk.21-school.ru>
2021-04-16 16:36:29 +0300

    Еще пара изменений

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-minikube start 
+#!/bin/sh
+minikube start --vm-driver=virtualbox
 minikube addons enable metallb;
 minikube addons enable dashboard;
 
-eval $(minikube docker-env);
-
 kubectl apply -f srcs/yaml/metallb-config.yaml;
 
+eval $(minikube -p minikube docker-env);
 docker build -t nginx:latest srcs/nginx/;
 kubectl apply -f srcs/yaml/nginx.yaml;
 
