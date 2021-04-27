e4176a13dd5d09d3f08f3ab4d9adf42f5fbbd24b
Slow Abra <sabra@at-p7.msk.21-school.ru>
2021-04-27 16:33:56 +0300

    Девопс для идиотов

--- srcs/mysql/init_database.sql
+++ srcs/mysql/init_database.sql
@@ -1,4 +1,3 @@
 CREATE DATABASE sabra_database;
-CREATE USER 'sabra'@'%' IDENTIFIED BY 'sabra_21';
-GRANT ALL PRIVILEGES ON sabra_database.* TO 'sabra'@'%';
+GRANT ALL PRIVILEGES ON sabra_database.* TO 'sabra'@'%'IDENTIFIED BY 'sabra_21';
 FLUSH PRIVILEGES;
--- srcs/mysql/start_db.sh
+++ srcs/mysql/start_db.sh
@@ -1,8 +1,18 @@
-rc default
-/usr/bin/telegraf &
+#!/bin/sh
+#rc default
+#/etc/init.d/mariadb setup
+#mysql -u root mysql < init_database.sql
+#mysql -u root sabra_database < db.sql
+#rc-service mariadb stop
+#supervisord -c /etc/supervisord.conf
+openrc default
+#openrc-init
+#openrc boot
+#touch /run/openrc/softlevel
 /etc/init.d/mariadb setup
-rc-service mariadb start
-mysql -u root mysql < init_database.sql
-mysql -u root sabra_database < db.sql
-rc-service mariadb stop
-supervisord -c /etc/supervisord.conf
+#mysql_install_db --datadir=/var/lib/mysql
+rc-service mariadb start 
+mysql -u root < init_database.sql
+#mysql -u root sabra_database  < /etc/db.sql 
+rc-service mariadb stop 
+/usr/bin/mysqld_safe 
