170074622371eb12ec5d9f40f41d180f12155119
Slow Abra <pobortcevv@gmail.com>
2021-04-30 14:25:40 +0300

    Резерв версия

--- srcs/mysql/init_database.sql
+++ srcs/mysql/init_database.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE sabra_database;
-GRANT ALL PRIVILEGES ON sabra_database.* TO 'sabra'@'%'IDENTIFIED BY 'sabra_21';
+CREATE USER 'sabra'@'localhost' IDENTIFIED BY 'sabra_21';
+GRANT ALL PRIVILEGES ON sabra_database.* TO 'sabra'@'localhost';
 FLUSH PRIVILEGES;
