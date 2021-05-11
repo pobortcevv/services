42f7be0e9489b013ddba970d5fd8bca598496549
Slow Abra <pobortcevv@gmail.com>
2021-05-11 18:02:24 +0300

    Исправлена ошибка в mysql, но по - прежнему бд не поднимается

--- srcs/mysql/start_db.sh
+++ srcs/mysql/start_db.sh
@@ -2,9 +2,7 @@
 rc default
 /etc/init.d/mariadb setup
 rc-service mariadb start
-
-mysql -u root mysql < create_db.sql
+mysql -u root mysql < init_database.sql
 #mysql -u root wordpress < mysql-service.sql
-
 rc-service mariadb stop
 supervisord -c /etc/supervisord.conf
